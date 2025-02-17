c68fb6b6834706e28b7ae0d5ad316e32775be858
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-17 21:26:24 +0400

    Board

--- src/main/java/com/leverx/course/entity/Board.java
+++ src/main/java/com/leverx/course/entity/Board.java
@@ -1,5 +1,8 @@
 package com.leverx.course.entity;
 
+import com.leverx.course.enums.Sign;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
@@ -7,6 +10,7 @@ public class Board {
     private char[][] board;
 
     public Board() {
+        board = new char[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = ' ';
@@ -31,15 +35,62 @@ public class Board {
         return true;
     }
 
-    public boolean makeMove() {
-
+    public void makeMove(int move, Sign sign) {
+        if (isValidMove(move)) {
+            int row = (move - 1) / 3;
+            int col = (move - 1) % 3;
+            board[row][col] = sign.getChar();
+        }
     }
 
+    //returns winner's sign
     public char checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0];
+            }
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i];
+            }
 
+            if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                return board[0][0];
+            }
+
+            if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+                return board[0][2];
+            }
+        }
+        return ' ';
     }
 
+    //returns available positions
     public List<Integer> getAvailableMoves() {
+        List<Integer> moves = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    int cellNumber = i * 3 + j + 1;
+                    moves.add(cellNumber);
+                }
+            }
+        }
+        return moves;
+    }
 
+    public void printInstructionBoard() {
+        System.out.println("""
+                | - | - | - |
+                | 1 | 2 | 3 |
+                | - | - | - |
+                | 4 | 5 | 6 |
+                | - | - | - |
+                | 7 | 8 | 9 |
+                | - | - | - |
+                """);
+    }
+
+    public char[][] getBoard() {
+        return board;
     }
 }
