5872cf303c85e14e0fef153d6d5d1949d1105b4f
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-17 22:03:54 +0400

    computer player

--- src/main/java/com/leverx/course/entity/Computer.java
+++ src/main/java/com/leverx/course/entity/Computer.java
@@ -3,11 +3,14 @@ package com.leverx.course.entity;
 import com.leverx.course.enums.Difficulty;
 import com.leverx.course.enums.Sign;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Computer extends Player{
+
+    private static final Logger log = Logger.getLogger(Computer.class.getName());
 
     private Difficulty difficulty;
 
@@ -24,12 +27,53 @@ public class Computer extends Player{
         return getBestMove(board);
     }
 
-    private int getBestMove(Board board) {
-        return 0;
-    }
-
     private int getRandomMove(Board board) {
         List<Integer> availableMoves = board.getAvailableMoves();
         return availableMoves.get(new Random().nextInt(availableMoves.size()));
     }
+
+    private int getBestMove(Board board) {
+
+        int bestScore = Integer.MIN_VALUE;
+        int bestMove = -1;
+
+        List<Integer> availableMoves = board.getAvailableMoves();
+
+        for (Integer move : availableMoves) {
+            Board tempBoard = getCopyOfBoard(board);
+            tempBoard.makeMove(move, getSign());
+
+            int score = miniMax(tempBoard, false); // after move, this checks possible outcomes (program is maximizer, user is minimizer)
+            log.info("Score while getting best move: " + score);
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        return bestMove;
+    }
+
+    private int miniMax(Board board, boolean isMaximizing) {
+        char winner = board.checkWinner();
+        if (winner == getSign().getChar()) return 1; // computer wins
+        if (winner != getSign().getChar() && winner != ' ') return -1; // computer loses
+        if (board.isFull()) return 0; // draw
+
+        int bestScore = isMaximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (Integer move : board.getAvailableMoves()) {
+            Board tempBoard = getCopyOfBoard(board);
+            tempBoard.makeMove(move, isMaximizing ? getSign() : (getSign() == Sign.X ? Sign.O : Sign.X));
+            int score = miniMax(board, !isMaximizing);
+            bestScore = isMaximizing ? Math.max(score, bestScore) : Math.min(score, bestScore);
+        }
+        return bestScore;
+    }
+
+    private Board getCopyOfBoard(Board board) {
+        Board copy = new Board();
+        copy.setBoard(Arrays.copyOf(board.getBoard(), board.getBoard().length));
+        return copy;
+    }
 }
+
